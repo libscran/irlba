1bd4b022ef406be5a37b4c7e9fdb366a114120d0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-04-14 00:30:07 +1000

    Fleshed out R comparison tests with more scenarios.

--- tests/R/tests/testthat/test-run.R
+++ tests/R/tests/testthat/test-run.R
@@ -8,7 +8,7 @@ compare <- function(left, right) {
 
 library(irlba)
 
-test_that("simple", {
+test_that("dense", {
     set.seed(10)
     mat <- matrix(rnorm(50000), 250, 200)
     set.seed(100)
@@ -28,18 +28,74 @@ test_that("simple", {
     compare(ref$u, out$U)
     compare(ref$v, out$V)
     expect_equal(ref$d, out$D)
+
+    # As DJ Khaled says, another one.
+    ref <- irlba::irlba(mat, nv=30, work=50, v=-v)
+    out <- run_irlba(mat, -v, number=30, work=20)
+
+    compare(ref$u, out$U)
+    compare(ref$v, out$V)
+    expect_equal(ref$d, out$D)
 })
 
-test_that("exact", {
+test_that("sparse", {
+    set.seed(20)
+    mat <- Matrix::rsparsematrix(100, 500, density=0.1)
+    set.seed(200)
+    v <- rnorm(ncol(mat))
+
+    ref <- irlba::irlba(mat, nv=5, work=15, v=v)
+    out <- run_irlba(mat, v, number=5, work=10)
+
+    compare(ref$u, out$U)
+    compare(ref$v, out$V)
+    expect_equal(ref$s, out$d)
+
+    # Another one, just for good measure. 
+    ref <- irlba::irlba(mat, nv=20, work=36, v=rev(v))
+    out <- run_irlba(mat, rev(v), number=20, work=16)
+
+    compare(ref$u, out$U)
+    compare(ref$v, out$V)
+    expect_equal(ref$d, out$D)
+
+    # As DJ Khaled says, another one.
+    ref <- irlba::irlba(mat, nv=40, work=70, v=-v)
+    out <- run_irlba(mat, -v, number=40, work=30)
+
+    compare(ref$u, out$U)
+    compare(ref$v, out$V)
+    expect_equal(ref$d, out$D)
+})
+
+test_that("exact small", {
     set.seed(20)
     mat <- matrix(rnorm(40), 5, 8)
     set.seed(200)
     v <- rnorm(ncol(mat))
 
-    ref <- irlba::irlba(mat, nv=2, work=10, v=v)
     out <- run_irlba(mat, v, number=2, work=2)
+    expect_identical(out$iterations, 0L)
+    expect_identical(out$multiplications, 0L)
 
+    ref <- irlba::irlba(mat, nv=2, work=10, v=v)
     compare(ref$u, out$U)
     compare(ref$v, out$V)
     expect_equal(ref$s, out$d)
+})
+
+test_that("exact large", {
+    set.seed(30)
+    mat <- matrix(rnorm(4000), 50, 80)
+    set.seed(200)
+    v <- rnorm(ncol(mat))
+
+    out <- run_irlba(mat, v, number=30, work=30)
+    expect_identical(out$iterations, 0L)
+    expect_identical(out$multiplications, 0L)
+
+    ref <- svd(mat)
+    compare(ref$u[,1:30], out$U)
+    compare(ref$v[,1:30], out$V)
+    expect_equal(ref$d[1:30], out$D)
 })
